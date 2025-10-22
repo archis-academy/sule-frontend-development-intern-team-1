32850afea327eb7b5eb8eff3df6cd00deccafe66
Yusuf Kaya <yusuf.kaya1059@gmail.com>
2025-10-22 03:02:32 +0300

    CTA banner has been created

---
+++ src/components/ctaBanner/ctaBanner.tsx
@@ -0,0 +1,25 @@
+import styles from "@components/ctaBanner/ctaBanner.module.scss";
+
+function CTABanner() {
+    return (
+        <section className={styles.ctaBanner}>
+            <img className={styles.vector1} src="/icons/vector-1.svg" alt="#" />
+            <div className={styles.ctaBannerContent}>
+                <div className={styles.photoWrapper}>
+                    <img className={styles.photo} src="/images/profile-photo.png" alt="profile-photo" />
+                </div>
+                <div className={styles.rightContent}>
+                    <div className={styles.textWrapper}>
+                        <h2 className={styles.title}>Become a Agent</h2>
+                        <p className={styles.text}>Fusce venenatis tellus a felis scelerisque. venenatis tellus a felis scelerisque.</p>
+                    </div>
+                    <button className={styles.registerButton}>Register Now</button>
+                </div>
+                <img className={styles.vector2} src="/icons/vector-2.svg" alt="#" />
+                <img className={styles.vector3} src="/icons/vector-3.svg" alt="#" />
+            </div>
+        </section>
+    )
+}
+
+export default CTABanner;
