87e9753f2872b02f6d71339c7703f1abff72fc4a
rumeysabyrk <rbayrak677@gmail.com>
2025-10-22 22:16:33 +0300

    fix

--- src/components/secondaryHeader/secondaryHeader.tsx
+++ src/components/secondaryHeader/secondaryHeader.tsx
@@ -8,32 +8,42 @@ import { CONTACT_INFO } from "@/constants/contact";
 const SecondaryHeader: React.FC = () => {
     return (
         <div className={styles["secondary-header"]}>
+
             <div className={styles["secondary-header__location"]}>
                 <div className={styles["secondary-header__icon"]}>
-                    <MapPin  />
+                    <MapPin />
                 </div>
                 <span className={styles["secondary-header__text"]}>
                     {CONTACT_INFO.location}
                 </span>
             </div>
+
             <div className={styles["secondary-header__contact"]}>
                 <div className={styles["secondary-header__phone"]}>
                     <div className={styles["secondary-header__phoneIcon"]}>
                         <Phone />
                     </div>
-                    <a href={`tel:${CONTACT_INFO.phone}`} className={styles["secondary-header__number"]}>
+                    <a
+                        href={`tel:${CONTACT_INFO.phone}`}
+                        className={styles["secondary-header__number"]}
+                    >
                         {CONTACT_INFO.phone}
                     </a>
                 </div>
+
                 <div className={styles["secondary-header__email"]}>
                     <div className={styles["secondary-header__emailIcon"]}>
                         <Mail />
                     </div>
-                    <a href={`mailto:${CONTACT_INFO.email}`} className={styles["secondary-header__emailText"]}>
+                    <a
+                        href={`mailto:${CONTACT_INFO.email}`}
+                        className={styles["secondary-header__emailText"]}
+                    >
                         {CONTACT_INFO.email}
                     </a>
                 </div>
             </div>
+
         </div>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "@pages/home/home";
-import SecondaryHeader from "@/components/secondaryHeader/secondaryHeader";
+import Layout from "@/layouts/layout";
 import "@src/main.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-       <SecondaryHeader />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
