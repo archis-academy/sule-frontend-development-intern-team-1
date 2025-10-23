4ef11b7e55370bcee7550b8acd5c4aa590a62698
Yusuf Kaya <yusuf.kaya1059@gmail.com>
2025-10-23 23:24:50 +0300

    changes have been fixed

--- src/components/locations/locations.tsx
+++ src/components/locations/locations.tsx
@@ -6,7 +6,7 @@ const locationsData = [
     { image: "/images/houston.jpg", no: 141, city: "Houston, TX" },
     { image: "/images/san-diego.jpg", no: 212, city: "San Diego, CA" },
     { image: "/images/philadelphia.jpg", no: 183, city: "Philadelphia, PA" },
-    { image: "/images/san-francisco.jpg", no: 112, city: "San Franciso, CA" },
+    { image: "/images/san-francisco.jpg", no: 112, city: "San Francisco, CA" },
 ]
 
 
@@ -15,8 +15,8 @@ function Locations() {
     return (
         <section className={styles.locations}>
             <div className={styles.titleWrapper}>
-                <span className={styles.secondaryTitle}>AREAS ACROSS THE TOWN</span>
-                <h2 className={styles.primaryTitle}>Neighborhood Properties</h2>
+                <h2 className={styles.secondaryTitle}>AREAS ACROSS THE TOWN</h2>
+                <span className={styles.primaryTitle}>Neighborhood Properties</span>
             </div>
             <div className={styles.locationsImageWrapper}>
                 {locationsData.map((data) => <LocationsCard key={data.no} {...data} />)}
