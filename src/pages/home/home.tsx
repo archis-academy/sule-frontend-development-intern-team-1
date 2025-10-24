838957b3f2244c1b738a8a42861c72019a05bfe0
rumeysabyrk <rbayrak677@gmail.com>
2025-10-24 16:47:28 +0300

    Implement hero left elements

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,10 +3,12 @@ import AboutSection from "@/components/aboutSection/aboutSection"
 import CTABanner from "@/components/ctaBanner/ctaBanner";
 import OurServices from "@/components/ourServices/ourServices";
 import Locations from "@/components/locations/locations";
+import HeroLeft from "@/components/heroLeft/heroLeft";
 
 function Home() {
   return (
     <>
+      <HeroLeft/>
       <AboutSection/>
       <CheckOutOurNew />
       <OurServices />
